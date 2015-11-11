fc621eeeab39c8e58b352f9aaf187e2416e19218
Michael Stucki <michael.stucki@typo3.org>
2015-11-11 15:20:15 +0100

    Improve logic in update-submodules.sh

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,20 +2,22 @@
 
 export LANG=C
 
-# Check if the repository is clean.
+# Check if the repository is clean at start.
 # If this is the case, changes will be committed automatically.
-REPOSITORY_IS_DIRTY_AT_START=0
-if [ $(git status --short | wc -l) != 0 ]; then
-	REPOSITORY_IS_DIRTY_AT_START=1
+REPOSITORY_IS_CLEAN_AT_START=0
+if [ $(git status --short | wc -l) = 0 ]; then
+	REPOSITORY_IS_CLEAN_AT_START=1
 fi
 
-# Check if the last commit was a submodule update and if it was not pushed yet.
+# Check if there are changes which were not pushed yet.
 # If this is the case, the last commit is amended at the end of the run.
-LAST_UPDATE_IS_PUSHED=1
-if [ $(git log --oneline origin/master..master | wc -l) != 0 ]; then
-	LAST_UPDATE_IS_PUSHED=0
+LAST_UPDATE_IS_PUSHED=0
+if [ $(git log --oneline origin/master..HEAD | wc -l) = 0 ]; then
+	LAST_UPDATE_IS_PUSHED=1
 fi
 
+# Check if the last commit was a submodule update.
+# If this is the case, the last commit is amended at the end of the run.
 LAST_COMMIT_IS_SUBMODULE_UPDATE=0
 if git log --oneline -n 1 HEAD | egrep -q "Update submodules$"; then
 	LAST_COMMIT_IS_SUBMODULE_UPDATE=1
@@ -26,12 +28,14 @@ git submodule foreach git pull
 echo
 echo "==========================================="
 
+# Check if the repository is clean (= if there were any changes).
+# If this is the case, stop here.
 if [ $(git status --short | wc -l) = 0 ]; then
 	echo "DONE - No changes."
 	exit 0
 fi
 
-if [ $REPOSITORY_IS_DIRTY_AT_START = 0 ]; then
+if [ $REPOSITORY_IS_CLEAN_AT_START = 1 ]; then
 	echo "DONE - Committing changes..."
 	git add .
 
